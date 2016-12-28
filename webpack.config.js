f9a285338df3e1be65780d9a6ca433ef4ee58de0
Daniel Ström <D@nielstrom.se>
2016-12-28 13:57:23 +0100

    Configure better source-maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  devtool: 'eval',
+  devtool: 'eval-source-map',
   entry: [
     // hot reload
     'react-hot-loader/patch',
