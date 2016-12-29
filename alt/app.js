b6680d4f281266659641c28dce06cdef44136630
Daniel Ström <D@nielstrom.se>
2016-12-29 08:20:21 +0100

    Define aditional quotes

--- alt/app.js
+++ alt/app.js
@@ -9,14 +9,18 @@ import texts from './texts.js';
 import Header from './header/header.js';
 
 import Quote from './quote/quote.js';
-import PA from './quote/per-arne.png';
+import imgOwner from './quote/owner.png';
+import imgClient from './quote/client.png';
+import imgAssistant from './quote/assistant.png';
 
 export default class App extends Component {
   render() {
     return (
       <main>
         <Header />
-        <Quote image={PA} quote={texts.quotes.PA.quote} caption={texts.quotes.PA.caption}/>
+        <Quote image={imgOwner} quote={texts.quotes.owner.quote} caption={texts.quotes.owner.caption}/>
+        <Quote image={imgClient} quote={texts.quotes.client.quote} caption={texts.quotes.client.caption}/>
+        <Quote image={imgAssistant} quote={texts.quotes.assistant.quote} caption={texts.quotes.assistant.caption}/>
       </main>
     );
   }
--- alt/texts.js
+++ alt/texts.js
@@ -1,8 +1,16 @@
 module.exports = {
   quotes: {
-    PA: {
+    owner: {
       quote: 'Hemmalaget är det personliga assistansbolaget som sätter dig och dina behov i fokus. Tillsammans ser vi till att du får den hjälp som du vill ha, utan onödigt krångel.',
       caption: 'Per-Arne, VD'
+    },
+    client: {
+      quote: 'Vi kompletterar varandra, min assistent och jag.',
+      caption: 'Andreas'
+    },
+    assistant: {
+      quote: 'Det är underbart att få göra skillnad i en människas liv.',
+      caption: 'Susanne, Assistent'
     }
   }
 }
