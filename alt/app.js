bea27d81038acdb96874e0db40cb2ab2f724d9b0
Daniel Ström <D@nielstrom.se>
2016-12-29 08:42:52 +0100

    Implement Regions-section

--- alt/app.js
+++ alt/app.js
@@ -13,12 +13,16 @@ import imgOwner from './quote/owner.png';
 import imgClient from './quote/client.png';
 import imgAssistant from './quote/assistant.png';
 
+import Regions from './regions/regions.js';
+
+
 export default class App extends Component {
   render() {
     return (
       <main>
         <Header />
         <Quote image={imgOwner} quote={texts.quotes.owner.quote} caption={texts.quotes.owner.caption}/>
+        <Regions />
         <Quote image={imgClient} quote={texts.quotes.client.quote} caption={texts.quotes.client.caption}/>
         <Quote image={imgAssistant} quote={texts.quotes.assistant.quote} caption={texts.quotes.assistant.caption}/>
       </main>
---
+++ alt/regions/regions.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import style from './regions.css';
+
+export default () => (
+  <section className={style.region}>
+    <h1 className={style.title}>Västra Götaland</h1>
+    <p className={style.text}>Sedan 2011 bedriver Hemmalaget Assistans AB verksamhet inom personlig assistans i Västra Götalandsregionen.</p>
+  </section>
+);
