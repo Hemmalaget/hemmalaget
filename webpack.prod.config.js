b0ba5c82c4b14dd0f74e920d6ff8867a7d399017
Daniel Ström <D@nielstrom.se>
2017-01-02 14:37:48 +0100

    Define optimized production build configuration

---
+++ webpack.prod.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: [
+    // Main entry
+    './app/index.js',
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/assets/'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': 'production'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin()
+  ],
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader?modules&importLoaders=1',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?bypassOnDebug&optimizationLevel=9&interlaced=false'
+        ]
+      }
+    ]
+  }
+}
