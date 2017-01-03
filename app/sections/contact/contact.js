577630680712bd2f283605c58673b94e80713d3c
Daniel Ström <D@nielstrom.se>
2017-01-03 16:57:07 +0100

    Use role and aria-label on image div

--- app/sections/contact/contact.js
+++ app/sections/contact/contact.js
@@ -5,7 +5,7 @@ import image from './natural_smile.jpg';
 export default () => (
   <section className={style.contact}>
     <div className={style.banner}>
-      <div className={style.image}></div>
+      <div className={style.image} role="image" aria-label="Vi hjälper dig!"></div>
       <div className={style.bar}>
         <h1 className={style.title}>Vi hjälper dig</h1>
         <a className={style.button} href="mailto:info@hemmalaget.se?Subject=Bli%20kund%20hos%20Hemmalaget">Kontakta oss!</a>
