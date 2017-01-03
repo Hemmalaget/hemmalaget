f3a23202f7f07ab138f55ac5f10ebbe6cbeba51e
Daniel Ström <D@nielstrom.se>
2017-01-03 16:49:48 +0100

    Use a terser component syntax

--- app/sections/header/header.js
+++ app/sections/header/header.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 import style from './header.css';
 
-export default class Header extends Component {
-  render() {
-    return (
-      <section className={style.header}>
-        <h1 className={style.title}>Personlig assistans<br /> på dina villkor</h1>
-      </section>
-    );
-  }
-}
+export default () => (
+  <section className={style.header}>
+    <h1 className={style.title}>Personlig assistans<br /> på dina villkor</h1>
+  </section>
+);
