c59d4b7a86e1f8600e1ca36a861da5838a999a53
Daniel Ström <D@nielstrom.se>
2018-03-03 10:55:11 +0100

    Remove image optimization as part of the build step

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,57 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack")
+const path = require("path")
 
 module.exports = {
-  devtool: 'eval-source-map',
+  devtool: "eval-source-map",
   entry: [
     // hot reload
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
+    "react-hot-loader/patch",
+    "webpack-dev-server/client?http://localhost:3000",
+    "webpack/hot/only-dev-server",
 
     // Main entry
-    './app/index.js',
+    "./app/index.js"
   ],
   output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/'
+    filename: "bundle.js",
+    path: path.join(__dirname, "dist"),
+    publicPath: "/"
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  plugins: [new webpack.HotModuleReplacementPlugin()],
   module: {
     rules: [
-      { test: /\.js$/,
+      {
+        test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: "babel-loader"
       },
       {
         test: /\.css$/,
         use: [
-          'style-loader',
+          "style-loader",
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               modules: true,
               importLoaders: 1
             }
           },
-          'postcss-loader'
+          "postcss-loader"
         ]
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              hash: 'sha512',
-              digest: 'hex',
-              name: '[hash].[ext]'
-            }
-          },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true,
-              mozjpeg: { progressive: true },
-              optipng: { optimizationLevel: 4 },
-              gifsicle: { interlaced: true },
-              pngquant: {
-                quality: '75-90',
-                speed: 5,
-              }
+              hash: "sha512",
+              digest: "hex",
+              name: "[hash].[ext]"
             }
           }
         ]
       }
     ]
   }
-};
+}
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,21 +1,21 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack")
+const path = require("path")
 
 module.exports = {
-  devtool: 'cheap-module-source-map',
+  devtool: "cheap-module-source-map",
   entry: [
     // Main entry
-    './app/index.js',
+    "./app/index.js"
   ],
   output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/'
+    filename: "bundle.js",
+    path: path.join(__dirname, "dist"),
+    publicPath: "/"
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
@@ -25,61 +25,43 @@ module.exports = {
   module: {
     rules: [
       {
-        enforce: 'pre',
+        enforce: "pre",
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'eslint-loader'
+        loader: "eslint-loader"
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader"
       },
       {
         test: /\.css$/,
         use: [
-          'style-loader',
+          "style-loader",
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               modules: true,
               importLoaders: 1
             }
           },
-          'postcss-loader'
+          "postcss-loader"
         ]
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              hash: 'sha512',
-              digest: 'hex',
-              name: '[hash].[ext]'
-            }
-          },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true
-              },
-              optipng: {
-                optimizationLevel: 9
-              },
-              gifsicle: {
-                interlaced: true
-              },
-              pngquant: {
-                quality: '75-90',
-                speed: 3,
-              }
+              hash: "sha512",
+              digest: "hex",
+              name: "[hash].[ext]"
             }
           }
         ]
       }
     ]
   }
-};
+}
