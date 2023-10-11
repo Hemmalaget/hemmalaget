4ac9b3c94aa898c6c43d890efbc0e10167b9c917
Daniel Ström <D@nielstrom.se>
2023-10-11 11:25:02 +0200

    Add Gasell logo

--- frontend/app/sections/Partners/index.tsx
+++ frontend/app/sections/Partners/index.tsx
@@ -8,6 +8,12 @@ export const Partners = () => (
     <div className={styles.logos}>
       <Image src="/fremia.png" alt="Fremia" width="100" height="100" />
       <Image src="/aiai.png" alt="AiAi" width="81" height="96" />
+      <Image
+        src="/gasell-logo-rgb-krans-2023.png"
+        alt="Gasell 2023"
+        width="67"
+        height="100"
+      />
     </div>
 
     <div>Vi innehar tillstånd från Inspektionen för Vård och Omsorg</div>
